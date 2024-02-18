b9a2cc8e7a6930ed7917c4eb238080aee68cd89e
David Hoffman <500149967@fvtc.edu>
2024-02-17 19:23:51 -0600

    Added welcome div

--- site.js
+++ site.js
@@ -1,1 +1,40 @@
-console.log(document.querySelector('p'))
+
+
+const hours = new Date().getHours() // get the current hour
+
+const isMorning = hours >= 4 && hours < 12 // is it morning?
+const isAfternoon = hours >= 12 && hours < 17 // is it afternoon?
+const isEvening = hours >= 17 || hours < 4 // is it evening?
+
+
+function createClock() {
+    const clocl = document.querySelector('.getclock')
+    const h2 = clock.querySelector('p')
+    if(isMorning)
+     {return "Have a Good Morning";}
+    else if(isAfternoon)
+    {return "Have a Good Morning";}
+    else if(isEvening)
+    {return "Have a Good Morning";}
+    
+}
+
+
+
+//document.getElementById("clock").innerHTML = createClock();
+
+
+/*
+function clock(){
+if(isMorning){
+    return "Have a Good Morning"
+}
+
+else if(isAfternoon){
+    return "Have a Good Morning"
+}
+//else if(){
+ //   h1.textContent = "Have a Good Morning"
+//}
+}
+
